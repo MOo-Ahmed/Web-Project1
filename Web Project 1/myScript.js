4055bc07451911671fb8d7fe57aa686e226bb034
Mohamed A. Abd El-Nabey <51803872+MOo-Ahmed@users.noreply.github.com>
2020-06-22 00:36:38 +0200

    finishing Sign up page

--- Web Project 1/myScript.js
+++ Web Project 1/myScript.js
@@ -13,3 +13,31 @@ function validateLoginForm()
     }
     return true;
 }
+
+function validateRegisterForm()
+{
+    var name = document.forms["register"]["name"].value;
+    var email = document.forms["register"]["email"].value;
+    var psw = document.forms["register"]["psw"].value;
+    var psw2 = document.forms["register"]["psw2"].value;
+    if(name.length <= 1){
+        alert("Invalid name !");
+        return false;
+    }
+    else if(email.indexOf(" ") != -1)
+    {
+       alert("Invalid email !");
+        return false ;
+    }
+    else if(psw.length < 8)
+    {
+       alert("Password should exceed 7 characters !");
+        return false ;
+    }
+    else if(psw !== psw2)
+    {
+       alert("Passwords don't match !");
+        return false ;
+    }
+    return true ;
+}
