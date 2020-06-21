853439c6060b1f066f43180a4eaf03d55f2165d0
Mohamed A. Abd El-Nabey <51803872+MOo-Ahmed@users.noreply.github.com>
2020-06-21 03:44:30 +0200

    Finishing login page

--- Web Project 1/myScript.js
+++ Web Project 1/myScript.js
@@ -1,4 +1,15 @@
 function validateLoginForm()
 {
-	
+	var email = document.forms["login"]["email"].value;
+    var psw = document.forms["login"]["psw"].value;
+    if (email.indexOf(" ") != -1) {
+        window.alert("Email shouldn't contain white space");
+        return false;
+    }
+    else if (psw.length < 8){
+        
+        window.alert("Password must be filled out");
+        return false;
+    }
+    return true;
 }
